59cae5aae683aa7014cbf55a84cf34da58269c0e
Ayyoubout <ayyouboutgouga@gmail.com>
2023-11-09 16:58:37 -0400

    Mise en place du menu et des pages de la partie front et en attente des images

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home, services, about, contact, register, login
 
 urlpatterns = [
 
     path('',home, name='Acceuil' ),
+    path('Services/',services, name='Services' ),
+    path('A_porpos/',about, name='A_propos' ),
+    path('Contact/',contact, name='Contact' ),
+    path('Register/',register, name='Register' ),
+    path('Login/',login, name='Login' ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,29 @@ from django.http import HttpResponse
 def home(request):
     
     return render(request, 'blog/index.html')
+
+
+def services(request):
+    
+    return render(request, 'blog/services.html')
+
+
+def about(request):
+    
+    return render(request, 'blog/about.html')
+
+
+def contact(request):
+    
+    return render(request, 'blog/contact.html')
+
+
+def login(request):
+    
+    return render(request, 'blog/login.html')
+
+
+def register(request):
+    
+    return render(request, 'blog/register.html')
     
